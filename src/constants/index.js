06a834f98de2d571572a27f1b3429a9db23af24d
suzilxptr <sushil.bastola@movial.com>
2019-03-08 14:22:49 +0200

    server url of local or remote

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,8 @@
-
+import Expo from "expo";
+const { manifest } = Expo.Constants;
+const LOCAL_BASE_URL = manifest.debuggerHost.split(`:`).shift().concat(`:3000`)
+  
 // server is running on heroku 
-export const BASE_URL = `https://protected-waters-18515.herokuapp.com/api/v1`;
+const HEROKU_BASE_URL = `https://protected-waters-18515.herokuapp.com/api/v1`;
+
+export const BASE_URL = HEROKU_BASE_URL || LOCAL_BASE_URL;
